4487dc7ef24c93c8a70075d5db9993bd7f2c0ccf
Matan Borenkraout <matanbobi@gmail.com>
2020-09-13 11:15:54 +0300

    first plugin test

--- plugins/plugin-chart-bar/src/entry.ts
+++
@@ -1,4 +0,0 @@
-import BarChartPlugin from './plugin';
-
-// configure the chart with the package name from package.json
-new BarChartPlugin().configure({ key: process.env.PACKAGE_NAME }).register();
