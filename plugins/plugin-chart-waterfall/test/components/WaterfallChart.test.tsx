37401134a8a8ccb15bf56009d576f76ab18d244e
amitmiran137 <amit.miran@nielsen.com>
2020-11-29 23:04:46 +0200

    fix: delete unused files

--- plugins/plugin-chart-waterfall/test/components/WaterfallChart.test.tsx
+++ plugins/plugin-chart-waterfall/test/components/WaterfallChart.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render, screen, configure, within } from '@testing-library/react';
+import { render, screen, configure } from '@testing-library/react';
 import { supersetTheme, ThemeProvider } from '@superset-ui/core';
 import WaterfallChart from '../../src/components/WaterfallChart';
 import transformProps from '../../src/plugin/transformProps';
