48830dfa70b6b75568841d9af73e6b03bb902f0b
Amit Miran <47772523+amitmiran137@users.noreply.github.com>
2020-12-17 19:28:37 +0200

    Update generateMafPreset.js

--- scripts/generateMafPreset.js
+++ scripts/generateMafPreset.js
@@ -16,7 +16,7 @@ const buildStringFromPackage = async packages => {
 };
 const pkgGlob = process.argv[2] || '*';
 
-const packages = fg.sync([`../../plugins/${pkgGlob}`], {
+const packages = fg.sync([`${process.env.GITHUB_WORKSPACE}/superset-viz-plugins/plugins/${pkgGlob}`], {
   onlyDirectories: true,
 });
 
