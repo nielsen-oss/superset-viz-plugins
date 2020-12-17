ba9ef630f7152fe098412ff3e2104bb8a543ff38
Amit Miran <47772523+amitmiran137@users.noreply.github.com>
2020-12-17 17:55:33 +0200

    Update prepare_build.sh

--- scripts/prepare_build.sh
+++ scripts/prepare_build.sh
@@ -3,17 +3,17 @@ set -e
 
 #migrated from old repo, probably not needed
 # yarn
-pwd
 
+echo $GITHUB_WORKSPACE
 
 # Add dependencies superset-frontend and generate the preset
-cp .npmrc ../../incubator-superset/superset-frontend/.npmrc
-cd ../../incubator-superset/superset-frontend
+cp .npmrc $GITHUB_WORKSPACE/incubator-superset/superset-frontend/.npmrc
+cd $GITHUB_WORKSPACE/incubator-superset/superset-frontend
 
 
 
-node ../../superset-viz-plugins/scripts/addDependencies.js
-node ../../superset-viz-plugins/scripts/generateMafPreset.js
+node $GITHUB_WORKSPACE/superset-viz-plugins/scripts/addDependencies.js
+node $GITHUB_WORKSPACE/superset-viz-plugins/scripts/generateMafPreset.js
 mv ./MafPreset.ts ./src/visualizations/presets/MafPreset.js
-cp ../../superset-viz-plugins/templates/setupPluginsExtra.js ./src/setup/setupPluginsExtra.js
+cp $GITHUB_WORKSPACE/superset-viz-plugins/templates/setupPluginsExtra.js ./src/setup/setupPluginsExtra.js
 npm install
