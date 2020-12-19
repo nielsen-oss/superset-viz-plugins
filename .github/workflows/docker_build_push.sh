2e795a0dcec56e2d02c6b5dceca4c95bd2005a20
Amit Miran <47772523+amitmiran137@users.noreply.github.com>
2020-12-19 14:29:08 +0200

    Create docker_build_push.sh

---
+++ .github/workflows/docker_build_push.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+
+SHA=$(git rev-parse HEAD)
+TAG_NAME="nielsenoss/apache-superset"
+
+#
+# Build the  image
+#
+docker build \
+  -t "${TAG_NAME}:0.38" \
+  --label "sha=${SHA}" \
+  --label "built_at=$(date)" \
+  --label "build_actor=${GITHUB_ACTOR}" \
+  .
+
+
+if [ -z "${DOCKERHUB_TOKEN}" ]; then
+  # Skip if secrets aren't populated -- they're only visible for actions running in the repo (not on forks)
+  echo "Skipping Docker push"
+else
+  # Login and push
+  docker logout
+  docker login --username "${DOCKERHUB_USER}" --password "${DOCKERHUB_TOKEN}"
+  docker push "${TAG_NAME}"
+fi
