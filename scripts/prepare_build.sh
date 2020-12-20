e755d6d641109e592304d08df353d8dd49cf7bcd
Amit Miran <47772523+amitmiran137@users.noreply.github.com>
2020-12-20 18:15:33 +0200

    chore: cleanup prepare_build.sh script

--- scripts/prepare_build.sh
+++ scripts/prepare_build.sh
@@ -1,24 +1,20 @@
 #!//bin/bash
 set -e
 
-#migrated from old repo, probably not needed
-# yarn
-
 echo $GITHUB_WORKSPACE
 
 # Add dependencies superset-frontend and generate the preset
 cp .npmrc $GITHUB_WORKSPACE/incubator-superset/superset-frontend/.npmrc
 cd $GITHUB_WORKSPACE/incubator-superset/superset-frontend
 
-
-
+# add dependecies to pacakge.json
 node $GITHUB_WORKSPACE/superset-viz-plugins/scripts/addDependencies.js
-cat $GITHUB_WORKSPACE/incubator-superset/superset-frontend/package.json
 
+# generate preset file and locate in incubator source code
 node $GITHUB_WORKSPACE/superset-viz-plugins/scripts/generateMafPreset.js
 mv ./MafPreset.ts ./src/visualizations/presets/MafPreset.js
-cat $GITHUB_WORKSPACE/incubator-superset/superset-frontend/src/visualizations/presets/MafPreset.js
 
+# override setupPluginsExtra.js in incubator source code
 cp $GITHUB_WORKSPACE/superset-viz-plugins/templates/setupPluginsExtra.js ./src/setup/setupPluginsExtra.js
-cat $GITHUB_WORKSPACE/incubator-superset/superset-frontend/src/setup/setupPluginsExtra.js
+
 npm install
