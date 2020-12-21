d88706538a1c4bcf4ee6d359c0811cf9450a34c1
amitmiran137 <amit.miran@nielsen.com>
2020-12-21 22:17:23 +0200

    chore: cleanup debugging

--- scripts/prepare_build.sh
+++ scripts/prepare_build.sh
@@ -7,18 +7,16 @@ echo $GITHUB_WORKSPACE
 # Add dependencies superset-frontend and generate the preset
 cp .npmrc $GITHUB_WORKSPACE/incubator-superset/superset-frontend/.npmrc
 cd $GITHUB_WORKSPACE/incubator-superset/superset-frontend
-ls ./src/setup
+
 # add dependecies to pacakge.json
 node $GITHUB_WORKSPACE/superset-viz-plugins/scripts/addDependencies.js
 
 # generate preset file and locate in incubator source code
 node $GITHUB_WORKSPACE/superset-viz-plugins/scripts/generatePreset.js
-cat ./NielsenPreset.ts
 mv ./NielsenPreset.ts ./src/visualizations/presets/NielsenPreset.js
 
-# override setupPluginsExtra in incubator source code
+# generate setupPluginsExtra override in incubator source code
 node $GITHUB_WORKSPACE/superset-viz-plugins/scripts/generateSetupPluginsExtra.js
-cat "./$PLUGINS_EXTRA_FILENAME"
 cp "./${PLUGINS_EXTRA_FILENAME}" "src/setup/${PLUGINS_EXTRA_FILENAME}"
 
 npm install
