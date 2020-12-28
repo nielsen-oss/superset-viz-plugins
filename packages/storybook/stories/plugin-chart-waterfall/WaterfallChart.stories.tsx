a2889d32ae8f89b7bdd1355e5a507b217ccd99a9
Simcha Shats <simcha.shats@nielsen.com>
2020-12-28 16:44:52 +0200

    feat: add storybook

--- packages/storybook/stories/plugin-chart-waterfall/WaterfallChart.stories.tsx
+++ packages/storybook/stories/plugin-chart-waterfall/WaterfallChart.stories.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import WaterfallChart from '../../../../plugins/plugin-chart-waterfall/src/components/WaterfallChart';
 import transformProps from '../../../../plugins/plugin-chart-waterfall/src/plugin/transformProps';
-import {legendTop} from '../../../../plugins/plugin-chart-waterfall/test/__mocks__/waterfallProps';
-import {D3_FORMAT_OPTIONS,} from '@superset-ui/chart-controls';
-import {ChartProps} from "@superset-ui/core";
+import { legendTop } from '../../../../plugins/plugin-chart-waterfall/test/__mocks__/waterfallProps';
+import {
+  D3_FORMAT_OPTIONS,
+} from '@superset-ui/chart-controls';
 
 export default {
   title: 'Plugins/Waterfall Chart',
   component: WaterfallChart,
   argTypes: {
-    xAxisDataKey: {table: {disable: true}},
-    dataKey: {table: {disable: true}},
-    error: {table: {disable: true}},
-    onBarClick: {table: {disable: true}},
-    resetFilters: {table: {disable: true}},
-    data: {table: {disable: true}},
+    xAxisDataKey: { table: { disable: true } },
+    dataKey: { table: { disable: true } },
+    error: { table: { disable: true } },
+    onBarClick: { table: { disable: true } },
+    resetFilters: { table: { disable: true } },
+    data: { table: { disable: true } },
     numbersFormat: {
       control: {
         type: 'select',
@@ -26,11 +27,11 @@ export default {
 
 const Template = (args) => <WaterfallChart
   {...args}
-  data={transformProps({...legendTop, queriesData: args.queriesData} as unknown as ChartProps).data}
+  data={transformProps({ ...legendTop, queriesData: args.queriesData }).data}
 />;
 
 export const Default = Template.bind({});
 Default.args = {
-  ...transformProps(legendTop as unknown as ChartProps),
+  ...transformProps(legendTop),
   queriesData: legendTop.queriesData
 }
