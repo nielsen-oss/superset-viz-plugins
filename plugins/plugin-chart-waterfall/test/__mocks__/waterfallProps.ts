077ba202c99091d7642069f3c7fa6bf1d26c4611
Simcha Shats <simcha.shats@nielsen.com>
2020-12-30 12:23:42 +0200

    test: fix tests

---
+++ plugins/plugin-chart-waterfall/test/__mocks__/waterfallProps.ts
@@ -0,0 +1,57 @@
+export const legendTop = {
+  "formData": {
+    "xAxisColumn": "period_type",
+    "periodColumn": "period",
+    "metric": {
+      "expressionType": "SIMPLE",
+      "column": {
+        "id": 450,
+        "column_name": "volume_for_roi",
+        "verbose_name": null,
+        "description": null,
+        "expression": null,
+        "filterable": true,
+        "groupby": true,
+        "is_dttm": false,
+        "type": "DOUBLE PRECISION",
+        "python_date_format": null,
+        "optionName": "_col_volume_for_roi"
+      },
+      "aggregate": "COUNT",
+      "sqlExpression": null,
+      "isNew": false,
+      "hasCustomLabel": false,
+      "label": "COUNT(volume_for_roi)",
+      "optionName": "metric_hnkx1lvjdus_pwoc1ojw6vs"
+    },
+    "adhocFilters": [],
+    "rowLimit": 100,
+    "numbersFormat": null,
+    "legendPosition": "top"
+  },
+  "height": 400,
+  "queriesData": [{
+    "data": [{
+      "period_type": "Custom",
+      "period": "Total Period",
+      "COUNT(volume_for_roi)": 61
+    }, {"period_type": "Standard", "period": "Total Period", "COUNT(volume_for_roi)": 61}, {
+      "period_type": "Custom",
+      "period": "2018",
+      "COUNT(volume_for_roi)": 44
+    }, {"period_type": "Custom", "period": "2017", "COUNT(volume_for_roi)": 44}, {
+      "period_type": "Standard",
+      "period": "2018",
+      "COUNT(volume_for_roi)": 42
+    }, {"period_type": "Standard", "period": "2017", "COUNT(volume_for_roi)": 42}, {
+      "period_type": "Custom",
+      "period": "2020",
+      "COUNT(volume_for_roi)": 21
+    }, {"period_type": "Custom", "period": "2019", "COUNT(volume_for_roi)": 20}, {
+      "period_type": "Standard",
+      "period": "2019",
+      "COUNT(volume_for_roi)": 19
+    }, {"period_type": "Standard", "period": "2020", "COUNT(volume_for_roi)": 19}]
+  }],
+  "width": 800
+};
