1c2cecb2ed7dda5bc9063f3db40af11b747e6cf9
Simcha Shats <simcha.shats@nielsen.com>
2021-03-14 16:36:32 +0200

    feat: waterfall period

---
+++ plugins/plugin-chart-waterfall/src/components/WaterfallTooltip.tsx
@@ -0,0 +1,67 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+import React, { FC } from 'react';
+import { styled, t } from '@superset-ui/core';
+
+type WaterfallTooltipProps = {
+  formatter: Function;
+  active?: boolean;
+  payload?: { value: number[]; name: string }[];
+  label?: string;
+};
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr auto;
+  grid-gap: ${({ theme }) => theme.gridUnit * 3}px;
+  padding: ${({ theme }) => theme.gridUnit * 3}px;
+  background-color: ${({ theme }) => theme.colors.grayscale.light5};
+  border: 1px solid ${({ theme }) => theme.colors.grayscale.light1};
+`;
+
+const Title = styled.div`
+  grid-column: span 2;
+  font-weight: ${({ theme }) => theme.typography.weights.bold};
+`;
+
+const WaterfallTooltip: FC<WaterfallTooltipProps> = ({ active, payload, label, formatter }) => {
+  if (active && payload && payload.length) {
+    const lastPeriod = payload[0].value[0];
+    const thisPeriod = payload[0].value[1];
+    const change = thisPeriod - lastPeriod;
+    const dueTo = (change / lastPeriod) * 100;
+    return (
+      <Wrapper>
+        <Title>{label}</Title>
+        {!!lastPeriod && <div>{t('Last Period:')}</div>}
+        {!!lastPeriod && <div>{formatter(lastPeriod)}</div>}
+        <div>{t('This Period:')}</div>
+        <div>{formatter(thisPeriod)}</div>
+        {!!lastPeriod && <div>{t('Change:')}</div>}
+        {!!lastPeriod && <div>{formatter(change)}</div>}
+        {!!lastPeriod && <div>{t('Due-To %:')}</div>}
+        {!!lastPeriod && <div>{formatter(dueTo)}%</div>}
+      </Wrapper>
+    );
+  }
+
+  return null;
+};
+
+export default WaterfallTooltip;
