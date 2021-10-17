b9d76eb58ffcaa4f76776b0203e28724dcf994a6
Amit Miran <47772523+amitmiran137@users.noreply.github.com>
2021-10-17 17:21:51 +0300

    fix: trigger package bump

--- plugins/plugin-chart-composed/src/plugin/configs/colorScheme.ts
+++ plugins/plugin-chart-composed/src/plugin/configs/colorScheme.ts
@@ -89,6 +89,7 @@ const getColorSchemeByBreakdown = (i: number): [{ name: string; config: ControlC
   },
 ];
 
+
 const colorSchemeByBreakdown: (
   | [{ name: string; config: ControlConfig<'ColorSchemeControl'> }]
   | [{ name: string; config: ControlConfig<'AdhocFilterControl'> }]
@@ -96,6 +97,7 @@ const colorSchemeByBreakdown: (
 
 for (let i = 0; i < MAX_FORM_CONTROLS; i++) {
   colorSchemeByMetric.push(getColorSchemeBy(i));
+  
   colorSchemeByBreakdown.push(getColorSchemeByBreakdown(i));
 }
 
